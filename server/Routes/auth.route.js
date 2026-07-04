ad8a8d53532491496bbd9fcf32c26080a2b8d6dc
kisha-nkumar <kishan232632@gmail.com>
2026-07-04 12:22:09 +0530

    Update auth.route.js

--- server/Routes/auth.route.js
+++ server/Routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { googleAuth, logOut } from "../Controllers/auth.controller.js"
+import { googleAuth, logOut } from "../controllers/auth.controller.js"
 
 const authRouter = express.Router()
 
